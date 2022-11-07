d4b8f8efeb2c6e4b7527da79db988c44c79c714f
baischsoares <baischsoares@gmail.com>
2022-11-07 15:23:24 -0300

    Criaçao das labels de busca e ajuste na criaçao de vagas

--- module/criar-vagas.js
+++ module/criar-vagas.js
@@ -2,12 +2,15 @@
 export default function initCriarVagas() {
   const search = document.querySelector('#search')
   const jobsSection = document.getElementById('jobsArea')
-  
+  const vagasArray = []
+
+
   async function iniciarAsync(){
     const dadosResponse = await fetch('./data.json');
     const dadosJSON = await dadosResponse.json();
     dadosJSON.forEach(vaga => {
       criarCard(vaga)
+      vagasArray.push(vaga)
   })
   }
 
@@ -20,6 +23,38 @@ export default function initCriarVagas() {
     cardJob.querySelector('[data-contract]').innerHTML = vaga.contract
     cardJob.querySelector('[data-location]').innerHTML = vaga.location
 
+    const listaTeconologia = cardJob.querySelector('[data-tecnologias]')
+
+    const btnRole = document.createElement('button')
+    btnRole.setAttribute('data-role', '')
+    btnRole.innerHTML = vaga.role
+
+    const btnLevel = document.createElement('button')
+    btnRole.setAttribute('data-level', '')
+    btnRole.innerHTML = vaga.level
+
+    listaTeconologia.append(btnRole)
+
+    vaga.languages.forEach((language) => {
+      const listaTeconologiaCard = cardJob.querySelector('[data-tecnologias]')
+      const item = document.createElement('li')
+      const button = document.createElement('button')
+      button.setAttribute('data-language', '')
+      button.innerHTML = language
+      item.appendChild(button)
+      listaTeconologiaCard.append(item)
+    })
+
+    vaga.tools.forEach((tool) => {
+      const listaTeconologiaCard = cardJob.querySelector('[data-tecnologias]')
+      const item = document.createElement('li')
+      const button = document.createElement('button')
+      button.setAttribute('data-language', '')
+      button.innerHTML = tool
+      item.appendChild(button)
+      listaTeconologiaCard.append(item)
+    })
+
     if(vaga.new){
      const newText = document.createElement('span')
      newText.classList.add('new')
@@ -31,12 +66,10 @@ export default function initCriarVagas() {
       featured.classList.add('featured')
       featured.innerHTML = 'featured!'
       cardJob.querySelector('.tags').appendChild(featured)
-     }
+    }
 
 
      document.querySelector('.section-main').appendChild(cardJob)
-
-
   }
 
   iniciarAsync()
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import initCriarVagas from "./module/criar-vagas.js";
+import initCriarBuscas from "./module/criar-buscas.js";
+
 
 initCriarVagas()
+initCriarBuscas()
 
 
